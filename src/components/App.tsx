372d86a49d74edea7bdd90cf79b2b933e537d5e4
TomaszLaboj <tomasz.laboj@gmail.com>
2023-09-05 12:12:23 +0100

    added chakra

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import { MyComponent } from "./MyComponent";
 import "../styles/App.css";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
     return (
         <div className="App">
-            <MyComponent />
+            <ChakraProvider>
+                <MyComponent />
+            </ChakraProvider>
         </div>
     );
 }
